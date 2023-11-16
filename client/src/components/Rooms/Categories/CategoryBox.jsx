57c4879fe090caa20ca3734c181701bd18ccbbe8
mdabarik <mdabarik19@gmail.com>
2023-11-16 09:50:44 +0600

    query string params added

--- client/src/components/Rooms/Categories/CategoryBox.jsx
+++ client/src/components/Rooms/Categories/CategoryBox.jsx
@@ -1,8 +1,26 @@
-/* eslint-disable no-unused-vars */
+import { useNavigate, useSearchParams} from 'react-router-dom';
+import qs from 'query-string';
 
 const CategoryBox = ({label, icon:Icon }) => {
+    const [params, setParams] = useSearchParams();
+    const navigate = useNavigate();
+
+    const handleClick = () => {
+        let currentQuery = {};
+        if (params) {
+            currentQuery = qs.parse(params.toString());
+            const updatedQuery = {...currentQuery, category: label};
+            const url = qs.stringifyUrl({
+                url: '/',
+                query: updatedQuery
+            })
+            navigate(url);
+        }
+    }
+
+    console.log(params);
     return (
-        <div className="flex flex-col items-center justify-center gap-2 p-3 border-b-2 cursor-pointer">
+        <div onClick={() => handleClick(label)} className="flex flex-col items-center justify-center gap-2 p-3 border-b-2 cursor-pointer">
             <Icon></Icon>
             {label}
         </div>
